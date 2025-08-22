350a2b65b4c8a31ede910bfdf635bdd491e76eef
Francis <brakay1136@gmail.com>
2025-08-22 10:57:47 -0700

    html and css structure created

--- app.js
+++ app.js
@@ -1,3 +1,50 @@
+class TaskApp {
+  constructor() {}
+
+  async getTask() {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos?_limit=5"
+    );
+
+    return response.data;
+  }
+
+  displayTask() {
+    this.getTask().then((tasks) => {
+      for (const task of tasks) {
+        const li = document.createElement("li");
+        li.textContent = task.title;
+
+        document.getElementById("taskList").appendChild(li);
+      }
+    });
+  }
+
+  async addtask(task) {
+    const addTask = await axios.post(
+      "https://jsonplaceholder.typicode.com/todos",
+      { title: task.value, completed: false }
+    );
+
+    const newTask = addTask.data;
+    const taskList = document.getElementById("taskList");
+    const li = document.createElement("li");
+    li.textContent = newTask.title;
+
+    taskList.appendChild(li);
+  }
+}
+
+const task = new TaskApp();
+task.displayTask();
+
+const addTask = document.getElementById("addTask");
+const inputTask = document.getElementById("taskInput");
+addTask.addEventListener("click", () => {
+  task.addtask(inputTask);
+  inputTask.value = "";
+});
+
 axios
   .get("https://jsonplaceholder.typicode.com/todos")
   .then((res) => console.log(res.data))
